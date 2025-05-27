fce96fc350c1ab24b310463c7dabb30870d0adf9
parte <partaevil@gmail.com>
2025-05-27 23:35:15 +0300

    add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,71 @@
-import { ConstructorPage } from '@pages';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
+import {
+  ConstructorPage,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  Feed,
+  NotFound404
+} from '@pages';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, OrderInfo, Modal } from '@components';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <main>
+        <Routes>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal
+                title='Информация о заказе'
+                onClose={() => navigate('/feed')}
+              >
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='Детали ингредиента' onClose={() => navigate('/')}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal
+                title='Информация о заказе'
+                onClose={() => navigate('/profile/orders')}
+              >
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
