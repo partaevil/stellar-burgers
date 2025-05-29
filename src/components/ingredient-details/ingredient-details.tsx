580d437e7bfa3d94469d3bce85103a36a2b9ed10
parte <partaevil@gmail.com>
2025-05-29 13:26:23 +0300

    update store in burger related components

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,14 +1,33 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useSelector, useDispatch } from '../../services/store';
+import { fetchIngredients } from '../../services/slices/ingredientsSlice';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams<{ id: string }>();
+  const dispatch = useDispatch();
 
-  if (!ingredientData) {
+  const { ingredients, loading } = useSelector((state) => state.ingredients);
+
+  const ingredientData = ingredients.find(
+    (ingredient) => ingredient._id === id
+  );
+
+  useEffect(() => {
+    if (ingredients.length === 0) {
+      dispatch(fetchIngredients());
+    }
+  }, [dispatch, ingredients.length]);
+
+  if (loading || (!ingredientData && ingredients.length === 0)) {
     return <Preloader />;
   }
 
-  return <IngredientDetailsUI ingredientData={ingredientData} />;
+  if (!ingredientData && ingredients.length > 0) {
+    return <div>Ингредиент не найден</div>;
+  }
+
+  return <IngredientDetailsUI ingredientData={ingredientData!} />;
 };
