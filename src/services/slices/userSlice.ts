54a72922ffa88964034e07a0d31e4ceab4d83c52
parte <partaevil@gmail.com>
2025-05-29 13:25:10 +0300

    add slices

---
+++ src/services/slices/userSlice.ts
@@ -0,0 +1,64 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { TUser } from '@utils-types';
+import {
+  registerUserApi,
+  loginUserApi,
+  getUserApi,
+  updateUserApi,
+  logoutApi,
+  forgotPasswordApi,
+  resetPasswordApi
+} from '@api';
+
+type TUserState = {
+  user: TUser | null;
+  isAuthenticated: boolean;
+  isAuthChecked: boolean;
+  loginUserRequest: boolean;
+  loginUserError: string | null;
+  registerUserRequest: boolean;
+  registerUserError: string | null;
+};
+
+const initialState: TUserState = {
+  user: null,
+  isAuthenticated: false,
+  isAuthChecked: false,
+  loginUserRequest: false,
+  loginUserError: null,
+  registerUserRequest: false,
+  registerUserError: null
+};
+
+export const getUser = createAsyncThunk('user/getUser', async () => {
+  const response = await getUserApi();
+  return response.user;
+});
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setAuthChecked: (state) => {
+      state.isAuthChecked = true;
+    },
+    clearUserErrors: (state) => {
+      state.loginUserError = null;
+      state.registerUserError = null;
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isAuthenticated = true;
+        state.isAuthChecked = true;
+      })
+      .addCase(getUser.rejected, (state) => {
+        state.isAuthChecked = true;
+      });
+  }
+});
+
+export const { setAuthChecked, clearUserErrors } = userSlice.actions;
+export default userSlice.reducer;
