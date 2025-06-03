5a3e427de92016604fd5bb90ec4131aea0edfbe9
parte <partaevil@gmail.com>
2025-06-03 23:20:41 +0300

    update storybook for checking if __filename exist (for ci)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,13 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+let __dirname: string;
+if (typeof __filename === 'undefined') {
+  const __filename = fileURLToPath(import.meta.url);
+  __dirname = path.dirname(__filename);
+} else {
+  __dirname = path.dirname(__filename);
+}
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
