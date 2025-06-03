b9516617ae714c8b5ca2b3003e35b1f3059f725b
parte <partaevil@gmail.com>
2025-06-03 23:16:13 +0300

    update storybook conf

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,9 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -9,27 +13,97 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions'
   ],
+  typescript: {
+    check: false,
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+  babel: async (options) => {
+    return {
+      ...options,
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript'
+      ],
+      plugins: [
+        ...options.plugins,
+        ['module-resolver', {
+          root: ['./src'],
+          alias: {
+            '@pages': './src/pages',
+            '@components': './src/components',
+            '@ui': './src/components/ui',
+            '@ui-pages': './src/components/ui/pages',
+            '@utils-types': './src/utils/types',
+            '@api': './src/utils/burger-api.ts',
+            '@slices': './src/services/slices',
+            '@selectors': './src/services/selectors'
+          }
+        }]
+      ]
+    };
+  },
   webpackFinal: async (config) => {
-    config.resolve
-      ? (config.resolve.alias = {
-          ...config.resolve.alias,
-          '@pages': path.resolve(__dirname, '../src/pages'),
-          '@components': path.resolve(__dirname, '../src/components'),
-          '@ui': path.resolve(__dirname, '../src/components/ui'),
-          '@ui-pages': path.resolve(__dirname, '../src/components/ui/pages'),
-          '@utils-types': path.resolve(__dirname, '../src/utils/types'),
-          '@api': path.resolve(__dirname, '../src/utils/burger-api.ts'),
-          '@slices': path.resolve(__dirname, '../src/services/slices'),
-          '@selectors': path.resolve(__dirname, '../src/services/selectors')
-        })
-      : null;
+    if (config.module && config.module.rules) {
+      config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-typescript'
+              ],
+              plugins: [
+                ['module-resolver', {
+                  root: ['./src'],
+                  alias: {
+                    '@pages': path.resolve(__dirname, '../src/pages'),
+                    '@components': path.resolve(__dirname, '../src/components'),
+                    '@ui': path.resolve(__dirname, '../src/components/ui'),
+                    '@ui-pages': path.resolve(__dirname, '../src/components/ui/pages'),
+                    '@utils-types': path.resolve(__dirname, '../src/utils/types'),
+                    '@api': path.resolve(__dirname, '../src/utils/burger-api.ts'),
+                    '@slices': path.resolve(__dirname, '../src/services/slices'),
+                    '@selectors': path.resolve(__dirname, '../src/services/selectors')
+                  }
+                }]
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      });
+    }
+
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@pages': path.resolve(__dirname, '../src/pages'),
+        '@components': path.resolve(__dirname, '../src/components'),
+        '@ui': path.resolve(__dirname, '../src/components/ui'),
+        '@ui-pages': path.resolve(__dirname, '../src/components/ui/pages'),
+        '@utils-types': path.resolve(__dirname, '../src/utils/types'),
+        '@api': path.resolve(__dirname, '../src/utils/burger-api.ts'),
+        '@slices': path.resolve(__dirname, '../src/services/slices'),
+        '@selectors': path.resolve(__dirname, '../src/services/selectors')
+      };
+    }
+
     return config;
   },
   framework: {
     name: '@storybook/react-webpack5',
     options: {
       builder: {
-        useSWC: true
+        // disable SWC to use Babel instead
+        useSWC: false 
       }
     }
   },
@@ -37,4 +111,5 @@ const config: StorybookConfig = {
     autodocs: 'tag'
   }
 };
+
 export default config;
