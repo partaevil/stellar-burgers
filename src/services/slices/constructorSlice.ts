8e979db0f2b3391d6ff8901dca1edc1ca1750407
parte <partaevil@gmail.com>
2025-06-03 15:49:25 +0300

    randomuuid to uuid4v4

--- src/services/slices/constructorSlice.ts
+++ src/services/slices/constructorSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TConstructorIngredient, TIngredient } from '@utils-types';
+import { v4 as uuidv4 } from 'uuid';
 
 type TConstructorState = {
   bun: TConstructorIngredient | null;
@@ -24,7 +25,7 @@ const constructorSlice = createSlice({
         }
       },
       prepare: (ingredient: TIngredient) => ({
-        payload: { ...ingredient, id: crypto.randomUUID() }
+        payload: { ...ingredient, id: uuidv4() }
       })
     },
     removeIngredient: (state, action: PayloadAction<string>) => {
